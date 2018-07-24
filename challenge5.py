ff4dc9216544379857a577d836889f94bc7c51a9
ritoban123 <ritobanrc@gmail.com>
2018-07-24 08:22:05 +0530

    challenge 5 complete

---
+++ challenge5.py
@@ -0,0 +1,19 @@
+def gcd(a, b):
+    if a == 0:
+        return b
+    return gcd(b % a, a)
+
+
+def lcm(a, b):
+    return a*b/gcd(a,b)
+
+
+def main():
+    num = 1
+    for i in range(1, 20):
+        num = lcm(num, i)
+    print(num)
+
+
+if __name__ == '__main__':
+    main()
