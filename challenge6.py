6bd3fe45de90c09778a03d8743f3f9683416fac3
ritoban123 <ritobanrc@gmail.com>
2018-07-24 08:22:16 +0530

    challenge 6 complete

---
+++ challenge6.py
@@ -0,0 +1,12 @@
+def main():
+    squaresum = 0
+    sumsquare = 0
+    for i in range(1, 101):
+        squaresum += i
+        sumsquare += i*i
+    squaresum *= squaresum
+    print(squaresum - sumsquare)
+
+
+if __name__ == '__main__':
+    main()
